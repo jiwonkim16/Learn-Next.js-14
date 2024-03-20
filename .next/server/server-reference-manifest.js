06113217b0947bd8c48512f1a5d7072fb213270c
Kim Jiwon <stayby16@kakao.com>
2024-03-20 23:58:20 +0900

    🐛 npm permission error

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"InvHCA8JIS/1gCMg+L2RyzdPwlrKRh03KRS3FrTxf04=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"WNd+uAI7iI94dGJZIDwQnR63a60mOBWEZuNUB70KAe4=\"\n}"
